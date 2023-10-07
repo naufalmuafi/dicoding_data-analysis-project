dbce2d699eaadea85d7c41ddde6b40cdf9676523
Naufal Muafi <nmuafi1@gmail.com>
2023-10-07 16:45:15 +0700

    add: selling performance done

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -137,3 +137,43 @@ ax.tick_params(axis='y', labelsize=20)
 ax.tick_params(axis='x', labelsize=15)
 
 st.pyplot(fig)
+
+# Selling Performance
+st.subheader("Best & Worst Performing Product")
+
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(35,15))
+
+colors = ["#90CAF9", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3"]
+
+sns.barplot(
+  x="quantity_x",
+  y="product_name",
+  data=sum_order_items_df.head(5),
+  palette = colors,
+  ax=ax[0]
+)
+ax[0].set_ylabel(None)
+ax[0].set_xlabel("Number of Sales", fontsize=30)
+ax[0].set_title("Best Performing Product", loc="center", fontsize=50)
+ax[0].tick_params(axis='y', labelsize=35)
+ax[0].tick_params(axis='x', labelsize=30)
+
+sns.barplot(
+  x="quantity_x",
+  y="product_name",
+  data=sum_order_items_df.sort_values(by='quantity_x', ascending=True).head(5),
+  palette = colors,
+  ax=ax[1]
+)
+ax[1].set_ylabel(None)
+ax[1].set_xlabel("Number of Sales", fontsize=30)
+ax[1].invert_xaxis()
+ax[1].yaxis.set_label_position('right')
+ax[1].yaxis.tick_right()
+ax[1].set_title("Worst Performing Product", loc="center", fontsize=50)
+ax[1].tick_params(axis='y', labelsize=35)
+ax[1].tick_params(axis='x', labelsize=30)
+
+st.pyplot(fig)
+
+# Customers Demography
