160e426775b4ece7db4cc1e4bfcf695e23055917
Naufal Muafi <nmuafi1@gmail.com>
2023-10-07 17:20:21 +0700

    add: dashboard development done

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -241,3 +241,62 @@ st.pyplot(fig)
 st.subheader("Best Customer Based on RFM Parameters")
 
 col1, col2, col3 = st.columns(3)
+
+with col1:
+  avg_recency = round(rfm_df.recency.mean(), 1)
+  st.metric("Average Recency (days)", value=avg_recency)
+
+with col2:
+  avg_frequency = round(rfm_df.frequency.mean(), 2)
+  st.metric("Average Frequency", value=avg_frequency)
+
+with col3:
+  avg_frequency = format_currency(rfm_df.monetary.mean(), "AUD", locale="es_CO")
+  st.metric("Average Monetary", value=avg_frequency)
+
+fig, ax = plt.subplots(nrows=1, ncols=3, figsize=(35, 15))
+
+colors = ["#90CAF9", "#90CAF9", "#90CAF9", "#90CAF9", "#90CAF9"]
+
+sns.barplot(
+  y="recency",
+  x="customer_id",
+  data=rfm_df.sort_values(by="recency", ascending=True).head(5),
+  palette=colors,
+  ax=ax[0]
+)
+ax[0].set_ylabel(None)
+ax[0].set_xlabel("customer_id", fontsize=30)
+ax[0].set_title("By Recency (days)", loc="center", fontsize=50)
+ax[0].tick_params(axis='y', labelsize=30)
+ax[0].tick_params(axis='x', labelsize=35)
+
+sns.barplot(
+  y="frequency",
+  x="customer_id",
+  data=rfm_df.sort_values(by="frequency", ascending=False).head(5),
+  palette=colors,
+  ax=ax[1]
+)
+ax[1].set_ylabel(None)
+ax[1].set_xlabel("customer_id", fontsize=30)
+ax[1].set_title("By Frequency", loc="center", fontsize=50)
+ax[1].tick_params(axis='y', labelsize=30)
+ax[1].tick_params(axis='x', labelsize=35)
+
+sns.barplot(
+  y="monetary",
+  x="customer_id",
+  data=rfm_df.sort_values(by="monetary", ascending=False).head(5),
+  palette=colors,
+  ax=ax[2]
+)
+ax[2].set_ylabel(None)
+ax[2].set_xlabel("customer_id", fontsize=30)
+ax[2].set_title("By Monetary", loc="center", fontsize=50)
+ax[2].tick_params(axis='y', labelsize=30)
+ax[2].tick_params(axis='x', labelsize=35)
+
+st.pyplot(fig)
+
+st.caption('Copyright (c) Naufal Mu\'afi from Dicoding 2023')
